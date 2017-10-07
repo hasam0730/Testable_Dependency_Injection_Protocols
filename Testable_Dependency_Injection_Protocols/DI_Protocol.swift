c6dccdbeca26c9cac5b8f6a92a39e69721d4c2c2
BaoPT1 <BaoPT1@fsoft.com.vn>
2017-10-07 12:14:26 +0700

    adding file DI_protocol

---
+++ Testable_Dependency_Injection_Protocols/DI_Protocol.swift
@@ -0,0 +1,18 @@
+//
+//  DI_Protocol.swift
+//  Testable_Dependency_Injection_Protocols
+//
+//  Created by Phan That Bao on 10/7/17.
+//  Copyright © 2017 Phan That Bao. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct User {
+    let name: String
+    init(name: String) {
+        self.name = name
+    }
+}
+
